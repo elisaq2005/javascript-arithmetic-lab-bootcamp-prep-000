03640fb3eff2c448a3075f1864a9811d33ce5c15
elisaquiney@gmail.com <elisaquiney@gmail.com>
2018-03-24 23:38:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
 function add(a,b) {
   return a + b;
 }
+
+function subtract(a,b) {
+  return a- b;
+}
+
+function multiply(a,b) {
+  return a * b;
+}
+
+function divide(a,b) {
+  return a / b;
+}
+
+function inc(n) {
+  return n++;
+}
+
+function dec(n) {
+  return n--;
+}
+
+function makeInt(n) {
+  return parseInt(n);
+}
+
+function makeInt(n) {
+  return parseInt('2', 10);
+}
+
+function makeInt(n) {
+  return parseInt('none', 10);
+}
+
+function preserveDecimal (n) {
+  return parseFloat(n);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n,2);
+}
